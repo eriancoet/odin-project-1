d830141bca824017067b068c4534a7d01472116b
Rian Coetzee <eriancoet@gmail.com>
2024-07-18 19:12:23 +0200

    calculator

---
+++ calculator.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return 'Error: Division by zero';
+    }
+    return a / b;
+}
+
+
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+let displayValue = '';
+
+
+function operate(operator, a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
+
+
